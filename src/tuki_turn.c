788c0ec2dec6e1f11074b9feb3538adc538cb07b
scuola sisini <francescomichelesisini@gmail.com>
2018-05-13 12:39:04 +0200

    Add files via upload

---
+++ src/tuki_turn.c
@@ -0,0 +1,19 @@
+#include "player.h"
+#include <stdio.h>
+
+
+/*
+ *ImplemenT this function to capture giuli
+ *feel free ti use all you want
+ */
+
+void tuki_turn(){
+
+   player_rotate(DOWN);
+      
+    
+  /*step() function  gives the control to Giuli
+   *the instructions under it will not be executed 	
+   */	
+  step();
+  }
